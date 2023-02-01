0e28a20287e4cdb82d83ac857afa2935641fced8
maazster72 <mabdulahmed02@gmail.com>
2023-02-01 22:21:11 +0000

    new modal component

--- practice/my-first-react-app/src/App.js
+++ practice/my-first-react-app/src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo text='Eat Food'/>
+      <Todo text='Sleep'/>
+      <Todo text='Go to the bathroom'/>
     </div>
   );
 }
