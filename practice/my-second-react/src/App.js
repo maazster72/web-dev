7e63e19b3d4be9331d0642127b6d321756eba8fa
maazster72 <mabdulahmed02@gmail.com>
2023-02-02 23:13:45 +0000

    navigation added

--- practice/my-second-react/src/App.js
+++ practice/my-second-react/src/App.js
@@ -3,14 +3,18 @@ import { Route,Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavouritesPage from "./pages/Favourites";
 import NewMeetupsPage from "./pages/NewMeetups";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
+    <><MainNavigation />
+    <div>
       <Routes>
-        <Route path="/" element={<AllMeetupsPage />}/>
-        <Route path='/favourites' element={<FavouritesPage />}/>
-        <Route path='/new-meetup' element={<NewMeetupsPage />}/>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path='/favourites' element={<FavouritesPage />} />
+        <Route path='/new-meetup' element={<NewMeetupsPage />} />
       </Routes>
+    </div></>
   );
 }
 
